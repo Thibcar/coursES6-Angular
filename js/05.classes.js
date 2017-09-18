8a6612542186073aa6a582df0a0ac999aad90091
unknown <thibault.carrasse@gmail.com>
2017-09-18 12:39:58 +0200

    Classes javascript

--- js/05.classes.js
+++ js/05.classes.js
@@ -34,3 +34,49 @@ class Vehicule {
 let myVehicule = new Vehicule("Mercedes", "Black");
 console.log(myVehicule);
 myVehicule.klaxonne();
+
+class Accessoires {
+     
+}
+
+
+
+console.log('____________________________________________________________________________________________');
+
+// héritage avec ES6
+
+class VoitureDeCourse extends Vehicule {
+    constructor (brand, color, vitesse) {
+        super(brand,color); // appel constructeur parent
+        this.vitesse = vitesse;
+    }
+
+    turbo() {
+        console.log('Mise en route du turbo');
+    }
+
+    // surcharge du klaxonne
+
+    klaxonne() {
+        console.log('Pouet Pouet');
+    }
+}
+
+let myFerrrari = new VoitureDeCourse("Ferrari", "rouge", 250);
+console.log(myFerrrari);
+myFerrrari.klaxonne();
+
+
+console.log('____________________________________________________________________________________________');
+
+class Supercar extends VoitureDeCourse {
+    constructor(nbCylindres) {
+        super('Lamborghini', 'orange', 350);
+        this.nbCylindres = nbCylindres;
+    }
+}
+
+let mySupercar = new Supercar(12);
+console.log(mySupercar);
+mySupercar.klaxonne();
+mySupercar.turbo();
